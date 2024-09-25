e6149df018788c4951368e97540c09e46805910d
Doragon05 <pyskid@gmail.com>
2024-09-25 14:31:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,6 @@ function setupMenuLinks() {
     document.getElementById('calendar-link').href = 'https://doragon05.github.io/kalendarz_gmin/';
     document.getElementById('schedule-link').href = 'https://greenmax2020.sharepoint.com/:x:/s/dzial-terenowych-sil-sprzedazy/ETzLx_rPRsFNt1ooG8RK0_4BLarL4zLC5eYuqq9gZu1Lgw'; 
     document.getElementById('report-link').href = 'https://doragon05.github.io/kalendarz_gmin/raporty.html'; 
-    document.getElementById('range-link').href = 'https://doragon05.github.io/kalendarz_gmin/zasieg/zasieg.html'; 
+    document.getElementById('range-link').href = 'https://doragon05.github.io/kalendarz_gmin/zasieg.html'; 
 }
 document.addEventListener('DOMContentLoaded', setupMenuLinks);
